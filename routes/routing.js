3c93ce0cb876c77f3591494753f5a894eca08f15
kcshamil <shamilshifu6@gmail.com>
2026-01-26 13:53:04 +0530

    Add: download recipe & middleware

--- routes/routing.js
+++ routes/routing.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const recipeController = require('../controller/recipeController')
 const userController = require('../controller/userController')
+const downloadController = require('../controller/downloadController')
+const jwtMiddleware = require('../middleware/jwtMiddleware')
 
-
-const router = express.Router()
+const router = new express.Router()
 
 // get all recipes
 router.get('/recipes', recipeController.getAllRecipesController)
@@ -11,11 +12,14 @@ router.get('/recipes', recipeController.getAllRecipesController)
 router.post('/register', userController.registerController)
 // login
 router.post('/login', userController.loginController)
+
+// ---------------Authorised user----------------
 // view recipe
 router.get('/recipes/:id', recipeController.viewRecipeController)
 // get related recipe
 router.get('/related-recipes', recipeController.relatedRecipesRecipeController )
-
+// addtodownload
+router.post('/downloads/:id', jwtMiddleware,downloadController.addToDownloadController )
 
 
 module.exports = router
